d1a1a4c380ef3b413cbf46cc5fa3811c3f0c8eb6
Vladimir Petko <vladimir.petko@canonical.com>
2025-01-16 15:03:26 +1300

    fix: use build-test-images.sh script in pipeline

--- tests/build-test-images.sh
+++ tests/build-test-images.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
-set -e 
+set -e
 
-(cd jre/ubuntu-24.04-headless && \
-    rockcraft pack --build-for amd64 && \
-    rockcraft.skopeo copy oci-archive:jre_21-edge_amd64.rock docker-daemon:ubuntu/jre:test)
+ARCH=${1:-amd64}
+BASE_IMAGE=${2:-ubuntu/jre:test}
+BUILD_IMAGE=${3:-ubuntu/jre:test-builder}
+MAVEN_IMAGE=${4:-ubuntu/jre:test-maven}
+MAVEN_BASE_IMAGE=${5:-maven:3.9.9-eclipse-temurin-21}
+RELEASE=${5:-24.04}
+
+(cd jre/ubuntu-${RELEASE}-headless && \
+    rockcraft pack --build-for ${ARCH} && \
+    rockcraft.skopeo copy oci-archive:jre_21-edge_${ARCH}.rock docker-daemon:${BASE_IMAGE})
 
 docker build \
-    -t ubuntu/jre:test-builder \
+    -t ${BUILD_IMAGE} \
     --build-arg UID=$(id -u ${USER}) \
     --build-arg GID=$(id -g ${USER}) \
-    -f tests/containers/builder/Dockerfile.24.04 \
+    -f tests/containers/builder/Dockerfile.${RELEASE} \
     tests/containers/builder
 
 docker build \
-    -t ubuntu/jre:test-maven \
-    --build-arg ARCH=amd64 \
+    -t ${MAVEN_IMAGE} \
+    --build-arg ARCH=${ARCH} \
     --build-arg UID=$(id -u ${USER}) \
     --build-arg GID=$(id -g ${USER}) \
-    --build-arg BASE_IMAGE=ubuntu/jre:test \
-    --build-arg MAVEN_IMAGE=maven:3.9.9-eclipse-temurin-21 \
-    -f tests/containers/maven/Dockerfile.24.04 \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --build-arg MAVEN_IMAGE=${MAVEN_BASE_IMAGE} \
+    -f tests/containers/maven/Dockerfile.${RELEASE} \
     tests/containers/maven
