cb93cbbd9b59871befdca274ccc5b11246630cbf
Vladimir Petko <vladimir.petko@canonical.com>
2025-10-13 10:29:50 +1300

    fix: upadate maven tags

--- tests/build-test-images.sh
+++ tests/build-test-images.sh
@@ -4,13 +4,13 @@ set -e
 ARCH=${1:-amd64}
 BASE_IMAGE=${2:-ubuntu/jre:test}
 MAVEN_IMAGE=${4:-ubuntu/jre:test-maven}
-MAVEN_BASE_IMAGE=${5:-maven:3.9.9-eclipse-temurin-25}
+MAVEN_BASE_IMAGE=${5:-maven:3.9-eclipse-temurin-25}
 RELEASE=${6:-24.04}
 
 (cd jre/ubuntu-${RELEASE}-headless && \
     rm -f *.rock && \
     rockcraft pack --build-for ${ARCH} && \
-    rockcraft.skopeo copy oci-archive:jre_*_${ARCH}.rock docker-daemon:${BASE_IMAGE})
+    rockcraft.skopeo copy oci-archive:jre_25-edge_${ARCH}.rock docker-daemon:${BASE_IMAGE})
 
 docker build \
     -t ${MAVEN_IMAGE} \
